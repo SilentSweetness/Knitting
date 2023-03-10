c2c967d829dae105cdac234ad4f7b6ff94c03eea
SilentSweetness <chelsey.lovendahl@gmail.com>
2023-03-10 09:17:40 -0500

    delete button

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const button = document.getElementById("enter");
 const input = document.getElementById("userinput");
-const ul = document.querySelector("ul")
+const ul = document.querySelector("ul");
+
+const deleteButton = document.getElementsByClassName("delete");
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
-        const li = document.createElement("li");
+        var li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
         input.value = "";
+        var deleteButton = document.createElement("button");
+        deleteButton.setAttribute("class", "delete");
+        deleteButton.appendChild(document.createTextNode("Delete"));
+        li.appendChild(deleteButton);
+        deleteButton.addEventListener("click", function(e) {
+            e.target.parentNode.remove()
+          });
 }
 
 function addListAfterClick() {
-    if (inputLength() > 0) {
+        if (inputLength() > 0) {
         createListElement();
         }
 }
@@ -24,6 +33,15 @@ function addListAfterKeypress(event) {
             createListElement();
         }
     }
+
+
+    Array.prototype.slice.call(deleteButton).forEach(function(item) {
+        item.addEventListener("click", function(e) {
+          e.target.parentNode.remove()
+        });
+      
+      })
+
 
 button.addEventListener("click", addListAfterClick);
 
